072786ca8287c7829edcf94fb2248d52538bd312
Ciaran Oflannagain <oflannac@tcd.ie>
2024-01-14 17:56:41 +0000

    Adding Google Form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import GoogleSheetsTable from './GoogleSheetsTable'; // Adjust the path as needed
+import GoogleFormsEmbed from './GoogleFormEmbed'; // Adjust the path as needed
+import './App.css'; // Import the CSS file
 
-function App() {
+function Main() {
   return (
     <div>
       <h1 style={{ marginLeft: '20px', marginRight: '20px' }}>Dublin Boozer Tracker</h1>
-      <GoogleSheetsTable />
+      <div>
+        <Link to="/dublin_pub_tracker/submission">
+          <button className="button">Submit Pub Data</button>
+        </Link>
+        <GoogleSheetsTable />
+      </div>
     </div>
+  );
+}
+
+function Submission() {
+  return (
+    <div>
+      <h1>Contribute to the boozer tracker!</h1>
+      <Link to="/dublin_pub_tracker">
+        <button className="button return">Return to Main</button>
+      </Link>
+      <GoogleFormsEmbed />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/dublin_pub_tracker" element={<Main />} />
+        <Route path="/dublin_pub_tracker/submission" element={<Submission />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/GoogleFormEmbed.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function GoogleFormsEmbed() {
+  return (
+    <div>
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSegNQZQi2Hvt7bJs4hVEqfZaO4KF8BjIrle-PjvO5cSTC6nSw/viewform?usp=sf_link"
+        width="100%"
+        height="800px" // You can adjust the height as needed
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0"
+        title="Google Form"
+      >
+        Loading...
+      </iframe>
+    </div>
+  );
+}
+
+export default GoogleFormsEmbed;
